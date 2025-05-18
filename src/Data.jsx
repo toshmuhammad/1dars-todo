62b25335cf2caec3d8a40d15270ae68a1605a86c
toshmuhammad <toshmuhammadrafiqov@gmail.com>
2025-05-18 16:19:25 +0500

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "yousaf khan",
+        email: "yousaft1@gmail.com",
+        id: 1
+    },
+    {
+        name: "khalil",
+        email: "khalil@gmail.com",
+        id: 2
+    },
+    {
+        name: "Muhammad Yousaf",
+        email: "yousaft1@gmail.com",
+        id: 3
+    }
+]
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App'
+import UserReduser from './UserReduser'
+
+const store = configureStore({
+  reducer: {
+    users: UserReduser
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
